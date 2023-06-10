39635468690b17eef897bfa0a01b2b9310b5333e
Cuic <106545286+cfloresm003@users.noreply.github.com>
2023-06-09 20:47:13 -0600

    Create Ferreteria.java

---
+++ Main/Ferreteria.java
@@ -0,0 +1,11 @@
+package Main;
+
+import Vistas.*;
+
+public class Ferreteria {
+	public static void main(String[] args) {
+		V_Ferreteria ventanaFerreteria = new V_Ferreteria();
+		ventanaFerreteria.setVisible(true);
+		ventanaFerreteria.setLocationRelativeTo(null);
+    }
+}
